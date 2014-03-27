aeeef7661bd05d005efffdf3f75557cd28287f0c
Luc Thevenard <lucthevenard@gmail.com>
2014-03-26 22:08:08 -0400

    Add config files.

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jasmine: {
+      test: {
+        src: 'src/*.js',
+        options: {
+          specs: 'test/*.spec.js',
+          helpers: 'test/utils/*.js',
+          styles: 'dist/*.css',
+          keepRunner: true
+        }
+      }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: 'src/',
+          name: 'vendor/almond/almond',
+          include: ['<%= pkg.name %>'],
+          wrap: {
+            startFile: 'src/build/start.frag',
+            endFile: 'src/build/end.frag'
+          },
+          optimize: 'none',
+          out: './dist/<%= pkg.name %>.js'
+        }
+      }
+    },
+
+    // TODO: keep original banner comments.
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      my_target: {
+        files: {
+          './dist/<%= pkg.name %>.min.js': './dist/<%= pkg.name %>.js'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['src/*.js'],
+        tasks: ['default']
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-jasmine')
+  grunt.loadNpmTasks('grunt-contrib-requirejs')
+
+  grunt.registerTask('default', ['requirejs', 'uglify'])
+}
