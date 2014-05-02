ae178f2ebdbb4930e84cc6a8c1fe29c26d1fdf7d
Luc Thevenard <lucthevenard@gmail.com>
2014-05-02 12:07:13 -0400

    Replace new elements with spaces in inline mode.
    Closes #32.

--- src/inlineMode.js
+++ src/inlineMode.js
@@ -11,9 +11,19 @@ function onKeydown (e) {
 }
 
 function onInput () {
+  var first
+
   this.selection.save()
 
   this.sanitizer.clean(this.elem)
+
+  // Remove all whitespace at the beginning of the element.
+  first = this.elem.firstChild
+  while (this.node.isText(first) && !first.data) {
+    this.elem.removeChild(first)
+
+    first = this.elem.firstChild
+  }
 
   // Append a <br> if need be.
   if (!this.elem.textContent)
@@ -22,26 +32,47 @@ function onInput () {
   this.selection.restore()
 }
 
+/**
+ * insertSpaces() is a Sanitizer filter which inserts spaces between
+ * elements; this is so that, when pasting, "<p>One</p><p>Two</p>"" gets
+ * turned into "One Two", not "OneTwo".
+ *
+ * @param {Element} elem
+ */
+function insertSpaces (elem) {
+  var text
+
+  if (!this.selection.isMarker(elem) && elem.parentNode === this.elem &&
+      elem.previousSibling) {
+    text = document.createTextNode(' ')
+    this.elem.insertBefore(text, elem)
+  }
+}
+
 function InlineMode (Quill) {
 
   this.elem = Quill.elem
-  this.Quill = Quill
+  this.off = Quill.off.bind(Quill)
 
   // Store bound event handlers for later removal.
   this.onInput = onInput.bind(Quill)
+  this.insertSpaces = insertSpaces.bind(Quill)
 
   this.elem.addEventListener('keydown', onKeydown)
   Quill.on('input', this.onInput)
 
-  // Remove all (presumably) unwanted elements by simulating input.
+  Quill.sanitizer.addFilter(this.insertSpaces)
+
+  // Remove all (presumably) unwanted elements present on initialization
+  // by simulating input.
   this.onInput()
 }
 
 InlineMode.prototype.destroy = function () {
   this.elem.removeEventListener('keydown', onKeydown)
-  this.Quill.off('input', this.onInput)
+  this.off('input', this.onInput)
 
-  delete this.Quill
+  delete this.off
   delete this.elem
 
   return null
