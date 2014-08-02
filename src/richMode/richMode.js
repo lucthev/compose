35542038d6ef9ed847ac7c2e26c8ce4e005323e8
Luc Thevenard <lucthevenard@gmail.com>
2014-08-02 15:13:01 -0400

    Rich mode: setup
    Adds a module for “initializing” the editor when in rich mode; adding
    paragraph serializations to the view, etc. Currently requires that the
    editor have perfectly valid HTML, and at least one paragraph. This is
    too strict. At the very least, it should strip problematic whitespace.

--- src/richMode/richMode.js
+++ src/richMode/richMode.js
@@ -3,13 +3,15 @@
 var GetChildren = require('./getChildren'),
     Converter = require('./converter'),
     Selection = require('../selection'),
-    View = require('./view')
+    View = require('./view'),
+    Setup = require('./setup')
 
 function RichMode (Compose) {
   Compose.use(GetChildren)
   Compose.use(Selection)
   Compose.use(Converter)
   Compose.use(View)
+  Compose.use(Setup)
 }
 
 module.exports = RichMode
---
+++ src/richMode/setup.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * This module sets up the editor when in rich mode. Adds the paragraphs
+ * to the view, etc. Assumes a valid layout (i.e. something getChildren)
+ * won’t complain about.
+ *
+ * TODO: more leniency. Basic: putting in a paragraph if the editor is empty.
+ * Advanced: coercing lousy markup into something Compose can consume.
+ */
+function setup (Compose) {
+  var View = Compose.require('view'),
+      getChildren = Compose.require('getChildren'),
+      Converter = Compose.require('converter'),
+      errMsg = 'Failed to properly initialize Compose.',
+      numHrs = 0,
+      paragraphs,
+      children,
+      section,
+      i
+
+  paragraphs = Object.keys(Converter.allows)
+  paragraphs.push('hr')
+  paragraphs = paragraphs.join(',')
+  paragraphs = Compose.elem.querySelectorAll(paragraphs)
+
+  if (!paragraphs.length)
+    throw new Error('The editor must have at least one paragraph.')
+
+  for (i = 0; i < paragraphs.length; i += 1) {
+    if (paragraphs[i].nodeName === 'HR') {
+      section = Converter.toSectionObj(paragraphs[i].parentNode)
+      section.start = i - numHrs
+      View.sections.push(section)
+
+      numHrs += 1
+      continue
+    }
+
+    View.paragraphs.push(Converter.toParagraph(paragraphs[i]))
+  }
+
+  try {
+    children = getChildren()
+  } catch (e) {
+    throw new Error(errMsg)
+  }
+
+  if (children.length !== paragraphs.length - numHrs)
+    throw new Error(errMsg)
+}
+
+module.exports = setup
