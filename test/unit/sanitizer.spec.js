d91b37066bf24dcfa7d5f51555ad1600bd865038
Luc Thevenard <lucthevenard@gmail.com>
2015-03-06 21:36:26 -0500

    test: rework sanitizer tests

--- test/unit/sanitizer.spec.js
+++ test/unit/sanitizer.spec.js
@@ -1,25 +1,11 @@
-/* jshint mocha:true *//* global Compose, expect */
+/* jshint mocha:true *//* global Compose, formatBlock, expect, listPlugin */
 'use strict';
 
-chai.use(Superset)
-
-describe.skip('The sanitizer should', function () {
+describe('The sanitizer should', function () {
   var Serialize,
       sanitize
 
-  before(function (done) {
-    var elem = document.createElement('article'),
-        editor
-
-    document.body.appendChild(elem)
-    editor = new Compose(elem)
-
-    editor.use(function (Compose) {
-      sanitize = Compose.require('sanitize')
-      Serialize = Compose.require('serialize')
-      done()
-    })
-  })
+  before(init)
 
   it('extract paragraphs from a string of HTML', function () {
     var result = sanitize(
@@ -28,14 +14,10 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'p'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'p'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
   it('group stray inline elements into paragraphs', function () {
@@ -46,159 +28,189 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'Half n half',
-      type: 'p'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'p'
-    })
+    expect(result.paragraphs[0].text).to.equal('Half n half')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
-  it('ignore wrapping elements', function () {
+  it('ignore unknown elements', function () {
     var result = sanitize(
       '<article>' +
         '<div>' +
           '<p>One</p>' +
           '<div>' +
-            '<h2>Two</h2>' +
+            '<p>Two</p>' +
           '</div>' +
         '</div>' +
       '</article>'
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'p'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'h2'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
-  it('ignore empty paragraphs', function () {
+  it('insert BRs into empty paragraphs', function () {
     var result = sanitize(
       '<p>One</p>' +
       '<p></p>'
     )
 
-    expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'p'
-    })
+    expect(result.paragraphs.length).to.equal(2)
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('\n')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
-  it('ignore whitespace-only paragraphs', function () {
+  it('insert BRs into whitespace-only paragraphs', function () {
     var result = sanitize(
-      '<h2>One</h2>' +
-      '<p>    \n    </p>' +
-      '<p>Two</p>'
+      '<p>One</p>' +
+      '<p>        </p>'
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'h2'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'p'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('\n')
+    expect(result.paragraphs[1].type).to.equal('p')
+  })
+
+  it('remove extraneous whitespace', function () {
+    var result = sanitize(
+      '<p>          One        </p>'
+    )
+
+    expect(result.paragraphs.length).to.equal(1)
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
   })
 
   it('extract paragraphs from entire documents', function () {
-    // Microsoft Word sometimes does this.
+    // Microsoft Word sometimes does something similar when copy+pasting
     var result = sanitize(
       '<html>' +
         '<head>' +
           '<meta charset="UTF-8">' +
+          '<title>Test</title>' +
         '</head>' +
         '<body>' +
           '<div>' +
-            '<h2>One</h2>' +
-            '<blockquote>Two</blockquote>' +
+            '<p>One</p>' +
+            '<p>Two</p>' +
           '</div>' +
         '</body>' +
       '</html>'
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'h2'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'blockquote'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
-  it('treat children of blockquotes as blockquotes', function () {
+  it('correct invalid markup', function () {
     var result = sanitize(
-      '<blockquote>' +
-        '<p>One</p>' +
-        '<blockquote>Two</blockquote>' +
-      '</blockquote>'
-    )
-
-    expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'blockquote'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'blockquote'
-    })
-  })
-
-  it('treat children of list items as list items', function () {
-    var result = sanitize(
-      '<ol>' +
-        '<li>' +
-          '<p>One</p>' +
-          '<p>Two</p>' +
-        '</li>' +
-        '<li>Three</li>' +
-      '</ol>'
+      '<div>One<span><p>Two</p></span>Three</div>'
     )
 
     expect(result.paragraphs.length).to.equal(3)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'ol'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'ol'
-    })
-    expect(result.paragraphs[2]).to.superset({
-      text: 'Three',
-      type: 'ol'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
+    expect(result.paragraphs[2].text).to.equal('Three')
+    expect(result.paragraphs[2].type).to.equal('p')
   })
 
-  it('wrap non-<li> list children', function () {
+  it('account for styles in invalid markup', function () {
+    var result = sanitize('<div>One<strong><p>Two</p></strong>Three</div>')
+
+    expect(result.paragraphs.length).to.equal(3)
+    expect(result.paragraphs[0].markups).to.eql([])
+    expect(result.paragraphs[1].markups).to.eql([{
+      type: Serialize.types.bold,
+      start: 0,
+      end: 3
+    }])
+    expect(result.paragraphs[2].markups).to.eql([])
+  })
+
+  it('recognize invalid markup in lists', function () {
     var result = sanitize(
       '<ol>' +
-        'One' +
+        '<p>One</p>' +
         '<li>Two</li>' +
       '</ol>'
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'ol'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'ol'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('ol')
+  })
+
+  it('bubble down inline elements', function () {
+    var result = sanitize('<div><strong><div><p>One</p></div></strong></div>')
+
+    expect(result.paragraphs.length).to.equal(1)
+    expect(result.paragraphs[0].markups).to.eql([{
+      type: Serialize.types.bold,
+      start: 0,
+      end: 3
+    }])
+  })
+
+  it('bubble down inline elements (3)', function () {
+    var result = sanitize('<p>One</p><strong><p>Two</p></strong><p>Three</p>')
+
+    expect(result.paragraphs.length).to.equal(3)
+    expect(result.paragraphs[0].markups.length).to.eql(0)
+    expect(result.paragraphs[1].markups.length).to.eql(1)
+    expect(result.paragraphs[0].markups.length).to.eql(0)
+  })
+
+  /**
+   * Regression test: the sanitizer used to “unwrap” elements it didn’t
+   * recognize. The list plugin adds a handler for LIs, not their parent
+   * lists, but needs to know the type of the parent list. The sanitizer
+   * would unwrap ULs; the orphaned LIs would then default to ordered.
+   */
+  it('not unwrap elements (e.g. UL)', function () {
+    var result = sanitize(
+      '<ul><li>One</li></ul>'
+    )
+
+    expect(result.paragraphs.length).to.equal(1)
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('ul')
+  })
+
+  it('not unwrap elements (e.g. OL)', function () {
+    var result = sanitize(
+      '<ol><li>One</li></ol>'
+    )
+
+    expect(result.paragraphs.length).to.equal(1)
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('ol')
+  })
+
+  it('recognize the children of LIs as LIs', function () {
+    var result = sanitize(
+      '<ul><li><p>One</p>Two</li></ul>'
+    )
+
+    expect(result.paragraphs.length).to.equal(2)
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('ul')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('ul')
   })
 
   it('treat plain text as a paragraph', function () {
@@ -207,42 +219,45 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'p'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
   })
 
-  it('treat two <br>s as a new paragraph', function () {
+  it('split paragraphs at double newlines', function () {
     var result = sanitize(
       '<p>One<br><br>Two</p>'
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'p'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'p'
-    })
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
-  it('treat >= two <br>s as a new paragraph', function () {
+  it('leave trailing double newlines', function () {
     var result = sanitize(
-      '<blockquote>One<br><br><br>Two</blockquote>'
+      '<p>One<br><br></p>' +
+      '<p>Two</p>'
     )
 
     expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'blockquote'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Two',
-      type: 'blockquote'
-    })
+    expect(result.paragraphs[0].text).to.equal('One\n\n')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('Two')
+    expect(result.paragraphs[1].type).to.equal('p')
+  })
+
+  it('accurately split > 2 BRs', function () {
+    var result = sanitize(
+      '<p>One<br><br><br>Two</p>'
+    )
+
+    expect(result.paragraphs.length).to.equal(2)
+    expect(result.paragraphs[0].text).to.equal('One')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[1].text).to.equal('\nTwo')
+    expect(result.paragraphs[1].type).to.equal('p')
   })
 
   it('remove consecutive spaces', function () {
@@ -251,10 +266,8 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One Two',
-      type: 'p'
-    })
+    expect(result.paragraphs[0].text).to.equal('One Two')
+    expect(result.paragraphs[0].type).to.equal('p')
   })
 
   it('leave consecutive spaces in <pre> blocks', function () {
@@ -263,59 +276,8 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      // Note: middle space is a non-breaking space
-      text: 'One   Two',
-      type: 'pre'
-    })
-  })
-
-  it('apply smart text filters', function () {
-    var result = sanitize(
-      '<p>"Orange," replied Alex. "I <3 orange..."</p>'
-    )
-
-    expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      text: '“Orange,” replied Alex. “I ❤ orange…”',
-      type: 'p'
-    })
-  })
-
-  it('not apply smart text filters in <pre> blocks and <code> markups.', function () {
-    var result = sanitize(
-      '<pre>var x = 1 <3 ? "word" : ""</pre>' +
-      '<p>Arrow notation thing: <code>pointer->property</code></p>'
-    )
-
-    expect(result.paragraphs.length).to.equal(2)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'var x = 1 <3 ? "word" : ""',
-      type: 'pre'
-    })
-    expect(result.paragraphs[1]).to.superset({
-      text: 'Arrow notation thing: pointer->property',
-      type: 'p',
-      markups: [{
-        type: Serialize.types.code,
-        start: 22,
-        end: 39
-      }]
-    })
-  })
-
-  it('respect Compose-approved classes', function () {
-    var result = sanitize(
-      '<div>' +
-        '<blockquote class="pullquote">One</blockquote>' +
-      '</div>'
-    )
-
-    expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One',
-      type: 'pullquote'
-    })
+    expect(result.paragraphs[0].text).to.equal('One \u00A0 Two')
+    expect(result.paragraphs[0].type).to.equal('pre')
   })
 
   it('keep wanted markups', function () {
@@ -324,29 +286,13 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.paragraphs.length).to.equal(1)
-    expect(result.paragraphs[0]).to.superset({
-      text: 'One two three',
-      type: 'p',
-      markups: [{
-        type: Serialize.types.bold,
-        start: 4,
-        end: 7
-      }]
-    })
-  })
-
-  it('recognize <section>s as section starters', function () {
-    var result = sanitize(
-      '<p>One</p>' +
-      '<section>' +
-        '<p>Two</p>' +
-      '</section>'
-    )
-
-    expect(result.sections.length).to.equal(1)
-    expect(result.sections[0]).to.superset({
-      start: 1
-    })
+    expect(result.paragraphs[0].text).to.equal('One two three')
+    expect(result.paragraphs[0].type).to.equal('p')
+    expect(result.paragraphs[0].markups).to.eql([{
+      type: Serialize.types.bold,
+      start: 4,
+      end: 7
+    }])
   })
 
   it('recognize <hr>s as section starters', function () {
@@ -357,14 +303,12 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.sections.length).to.equal(1)
-    expect(result.sections[0]).to.superset({
-      start: 1
-    })
+    expect(result.sections[0].start).to.equal(1)
   })
 
   it('not keep sections starting at the same index', function () {
     var result = sanitize(
-      '<h2>One</h2>' +
+      '<p>One</p>' +
       '<section>' +
         '<hr>' +
         '<p>Two</p>' +
@@ -372,15 +316,13 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.sections.length).to.equal(1)
-    expect(result.sections[0]).to.superset({
-      start: 1
-    })
+    expect(result.sections[0].start).to.equal(1)
   })
 
-  it('recognize sections withing wrapping elements', function () {
+  it('recognize sections within wrapping elements', function () {
     var result = sanitize(
       '<div>' +
-        '<h2>One</h2>' +
+        '<p>One</p>' +
         '<section>' +
           '<p>Two</p>' +
           '<hr>' +
@@ -390,12 +332,8 @@ describe.skip('The sanitizer should', function () {
     )
 
     expect(result.sections.length).to.equal(2)
-    expect(result.sections[0]).to.superset({
-      start: 1
-    })
-    expect(result.sections[1]).to.superset({
-      start: 2
-    })
+    expect(result.sections[0].start).to.equal(1)
+    expect(result.sections[1].start).to.equal(2)
   })
 
   it('ignore trailing sections', function () {
@@ -415,4 +353,16 @@ describe.skip('The sanitizer should', function () {
 
     expect(result.sections.length).to.equal(0)
   })
+
+  function init () {
+    var elem = document.createElement('article')
+
+    document.body.appendChild(elem)
+    var editor = new Compose(elem)
+    editor.use(formatBlock)
+    editor.use(listPlugin)
+
+    sanitize = editor.require('sanitizer')
+    Serialize = editor.require('serialize')
+  }
 })
